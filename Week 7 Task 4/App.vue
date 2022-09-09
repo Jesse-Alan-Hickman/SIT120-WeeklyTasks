42e1fb2b4484982d199f9c8b320353c3c52ea876
Jesse-Alan-Hickman <jhickman@deakin.edu.au>
2022-09-09 15:38:56 +1000

    Attempt at Wek 7 Task 4
    Unsuccessful attempt at correctly incorporating slots

---
+++ Week 7 Task 4/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <my-component></my-component>
+</template>
+
+<script>
+import MyComponent from './components/my-component.vue';
+
+export default {
+    components: {
+    MyComponent
+},
+}
+</script>
+
+<style>
+    #app {
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        text-align: center;
+        color: blueviolet;
+        margin-top: 60px;
+    }
+</style>
---
+++ Week 7 Task 4/components/my-component.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1> Hello from my component!</h1>
+        <div> My name is {{ name }}</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: { type: String, default: "Jesse"}
+        }
+    }
+</script>
